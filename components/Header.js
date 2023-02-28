9afd5ff3c7b56de240ce570e983d78b681841bc1
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2023-02-28 23:02:05 +0300

    :sparkles: created header

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <header>
+            <nav>
+              <span>
+                <p className="navlink">Home</p>
+                <p className="navlink">About</p>
+              </span>
+              <span>
+                <h1>TranslateZen</h1>
+              </span>
+            </nav>
+            
+        </header>
+    );
+}
